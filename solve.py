670049976091eb2debe1bdecfad51edaf0cae785
Aatif Syed <aatif@aatifsyed.uk>
2020-09-08 02:43:42 +0100

    Make solution presentable

--- solve.py
+++ solve.py
@@ -1,20 +1,17 @@
 #!/bin/env python3
 # %%
-import json
+import argparse
 import itertools
-import re
+import json
 import logging as log
+import re
+
 from datetime import datetime
-
-import cProfile
-
-from typing import Dict, Any, Optional, Iterable, List
+from distutils.util import strtobool
+from pprint import pprint
+from typing import Dict, Any, Optional, Iterable, List, Generator
 
 # %%
-# Load our dictionary
-with open("words_alpha.txt") as txt:
-    dictionary: str = txt.read()
-
 # Which numbers correspond to which letters?
 CipherTable = Dict[int, Optional[str]]
 
@@ -24,35 +21,60 @@ WordDescriptor = List[int]
 # How often does each number appear in our word descriptors?
 FrequencyTable = Dict[int, int]
 
-blank_cipher_table: CipherTable = {number: None for number in range(1, 27)}
-
 # %%
 def possible_characters(cipher_table: CipherTable):
+    """What characters have not been filled in this table, in popularity order.
+
+    Args:
+        cipher_table (CipherTable): [description]
+
+    Yields:
+        [str]: character
+    """
     # Letters sorted by frequency
     for letter in "etaoinsrhdlucmfywgpbvkxqjz":
         if letter not in cipher_table.values():
             yield letter
 
 
-def iter_cipher(
-    cipher_table: CipherTable, frequency_table: FrequencyTable
-) -> Iterable[CipherTable]:
+def iter_cipher(cipher_table: CipherTable, frequency_table: FrequencyTable):
+    """Yields cipher tables that have the most frequently appearing key filled in (by a popular letter)
+
+    Args:
+        cipher_table (CipherTable): [description]
+        frequency_table (FrequencyTable): [description]
+
+    Yields:
+        [CipherTable]: [description]
+    """
+    # Take out the keys which have already been covered
     remaining_frequency_table: FrequencyTable = {
         key: value
         for key, value in frequency_table.items()
         if cipher_table[key] is None
     }
+    # Now get the most frequent
     best_value = 0
     best_key = 0
     for key, value in remaining_frequency_table.items():
         if value > best_value:
             best_key = key
             best_value = value
+
+    # Now iterate over characters that could fill that
     for character in possible_characters(cipher_table):
         yield {**cipher_table, **{best_key: character}}
 
 
 def get_frequency_table(word_descriptors: List[WordDescriptor]) -> FrequencyTable:
+    """Count the number of times each key appears in the puzzle
+
+    Args:
+        word_descriptors (List[WordDescriptor]): The puzzle
+
+    Returns:
+        FrequencyTable: [description]
+    """
     frequency_table = {number: 0 for number in range(1, 27)}
 
     for word_descriptor in word_descriptors:
@@ -65,6 +87,15 @@ def get_frequency_table(word_descriptors: List[WordDescriptor]) -> FrequencyTabl
 def decipher_to_regex(
     cipher_table: CipherTable, word_descriptor: WordDescriptor
 ) -> str:
+    """Turn a list of numbers into a regex, leaving [:alpha:] if the cipher isn't known for that key
+
+    Args:
+        cipher_table (CipherTable): [description]
+        word_descriptor (WordDescriptor): [description]
+
+    Returns:
+        str: [description]
+    """
     return "".join(
         map(
             lambda x: cipher_table[x] if cipher_table[x] is not None else "[a-z]",  # type: ignore
@@ -73,23 +104,35 @@ def decipher_to_regex(
     )
 
 
-def regex_in_dictionary(regex: str) -> bool:
-    global dictionary
-    if re.search(regex, dictionary):
-        return True
-    return False
-
-
 def deciphered_in_dictionary(
     cipher_table: CipherTable, word_descriptor: WordDescriptor
-):
+) -> bool:
+    """Could this word be deciphered into a valid dictionary word?
+
+    Args:
+        cipher_table (CipherTable): [description]
+        word_descriptor (WordDescriptor): [description]
+
+    Returns:
+        bool: [description]
+    """
+    global dictionary
     regex = decipher_to_regex(cipher_table, word_descriptor)
-    return regex_in_dictionary(regex)
+    return bool(re.search(regex, dictionary))
 
 
 def cipher_is_feasible(
     cipher_table: CipherTable, word_descriptors: List[WordDescriptor]
 ) -> bool:
+    """Given this cipher table, could all puzzle rows/columns be real words?
+
+    Args:
+        cipher_table (CipherTable): [description]
+        word_descriptors (List[WordDescriptor]): [description]
+
+    Returns:
+        bool: [description]
+    """
     for word_descriptor in word_descriptors:
         if not deciphered_in_dictionary(cipher_table, word_descriptor):
             return False
@@ -97,10 +140,17 @@ def cipher_is_feasible(
 
 
 def print_cipher(cipher_table: CipherTable):
-    print(datetime.now().strftime("%H:%M:%S"), end=" ")
-    for i in range(1, 27):
-        print(cipher_table[i] if cipher_table[i] is not None else "_", end="")
-    print()
+    """Utility function for conditionally printing
+
+    Args:
+        cipher_table (CipherTable): [description]
+    """
+    global args
+    if args.print:
+        print(datetime.now().strftime("%H:%M:%S"), end=" ")
+        for i in range(1, 27):
+            print(cipher_table[i] if cipher_table[i] is not None else "_", end="")
+        print()
 
 
 # %%
@@ -109,6 +159,16 @@ def solve(
     word_descriptors: List[WordDescriptor],
     frequency_table: FrequencyTable,
 ):
+    """Recurse/backtrack to find a solution. The whole point of this program.
+
+    Args:
+        cipher_table (CipherTable): [description]
+        word_descriptors (List[WordDescriptor]): [description]
+        frequency_table (FrequencyTable): [description]
+
+    Yields:
+        [type]: [description]
+    """
     if cipher_is_feasible(cipher_table, word_descriptors):
         if None not in cipher_table.values():
             # Found a full cipher table that works
@@ -121,18 +181,49 @@ def solve(
 
 
 # %%
-# Load in the puzzle
-with open("puzzler_codewords_volume_2/page_12.json") as jsonfile:
-    puzzle = json.load(jsonfile)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Backtracking solver for codewords")
+    parser.add_argument(
+        "--dictionary", default="words_alpha.txt", dest="dictionary_path"
+    )
+    parser.add_argument("--puzzle", required=True, dest="puzzle_path")
+    parser.add_argument(
+        "--print",
+        help="Print out progress as we backtrace",
+        dest="print",
+        action="store_true",
+    )
 
-# Can't map int -> str in JSON, so we used str -> str. Rectify that here
-known_keys = {int(key): value for key, value in puzzle["known_keys"].items()}
+    args = parser.parse_args(
+        ["--puzzle", "puzzler_codewords_volume_2/page_12.json", "--print"]
+        if "__IPYTHON__" in vars(__builtins__)
+        else None
+    )
 
-starting_table: CipherTable = {**blank_cipher_table, **known_keys}
-word_descriptors: List[WordDescriptor] = puzzle["words"]
-frequency_table: FrequencyTable = get_frequency_table(word_descriptors)
+    # Load in our dictionary
+    with open(args.dictionary_path) as txt:
+        dictionary: str = txt.read()
 
-# %%
-cProfile.run("next(solve(starting_table, word_descriptors, frequency_table))")
+    # Load in our puzzle
+    with open(args.puzzle_path) as jsonfile:
+        puzzle = json.load(jsonfile)
+
+    # Can't map int -> str in JSON, so we used str -> str. Rectify that here
+    known_keys = {int(key): value for key, value in puzzle["known_keys"].items()}
+
+    blank_cipher_table: CipherTable = {number: None for number in range(1, 27)}
+    starting_table: CipherTable = {**blank_cipher_table, **known_keys}
+    word_descriptors: List[WordDescriptor] = puzzle["words"]
+    frequency_table: FrequencyTable = get_frequency_table(word_descriptors)
+
+    for solution in solve(starting_table, word_descriptors, frequency_table):
+        print("Found solution:")
+        pprint(solution)
+        cont: str = input("Continue search?")
+        cont: bool = bool(strtobool(cont))
+        if cont:
+            continue
+        else:
+            break
 
 # %%
